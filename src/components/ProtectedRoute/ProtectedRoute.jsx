d54541106bde52864918bcc7872b38d3c0ea814d
gsPatrick <patrickgsiqueira@hotmail.com>
2025-04-22 09:42:57 -0300

    login

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = () => {
 
   // If token exists, allow access to the nested routes (render the Outlet)
   // If no token, redirect the user to the root page ('/')
-  return token ? <Outlet /> : <Navigate to="/" replace />;
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
   // 'replace' ensures the login page replaces the current entry in history,
   // so the user doesn't go back to the protected route by clicking the back button.
 };
